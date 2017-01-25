44a3aae136c0b0d5b51ade284ae675c9adf1cf05
Ben Hadfield <Ben@hadfieldfamily.co.uk>
2017-01-25 16:07:14 +0000

    completed react-router setup

--- client/app.jsx
+++ client/app.jsx
@@ -9,8 +9,8 @@ import {Router, browserHistory} from "react-router";
 import {createStore} from "redux";
 import {Provider} from "react-redux";
 import {notify} from "react-notify-toast";
-import "./styles/base.css";
 import rootReducer from "./reducers";
+import "./styles/base.css";
 
 // Add the client app start up code to a function as window.webappStart.
 // The webapp's full HTML will check and call it once the js-content
--- client/routes.jsx
+++ client/routes.jsx
@@ -1,14 +1,12 @@
 import React from "react";
-import {Router, Route, hashHistory} from "react-router";
+import {Router, Route} from "react-router";
 import Home from "./components/home";
 import Jobs from "./components/jobs"
 import Scheduler from "./components/scheduler"
 
 export const routes = (
-  <Router history={hashHistory}>
-    <Route path="/" component={Home}>
-      <Route path="/scheduler" component={Scheduler}/>
-      <Route path="/jobs" component={Jobs}/>
-    </Route>
-  </Router>
+  <Route path="/" component={Home}>
+    <Route path="/scheduler" component={Scheduler}/>
+    <Route path="/jobs" component={Jobs}/>
+  </Route>
 );
