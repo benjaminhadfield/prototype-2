aa390da2796f478049570e7f5efe21764af2b5de
Ben Hadfield <Ben@hadfieldfamily.co.uk>
2017-03-08 14:31:28 +0000

    removed .build from git

--- server/api/database.js
+++ server/api/database.js
@@ -8,7 +8,8 @@ var connect = function() {
     host     : 'localhost',
     user     : 'root',
     password : 'root',
-    database : 'peach'
+    database : 'peach',
+    port: 8889
   });
   pool.getConnection(function(err, connection) {
     if (err) {
