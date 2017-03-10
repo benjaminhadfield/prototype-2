c827770bb1f2de8e0855ff2939a7565856f52997
Ben Hadfield <Ben@hadfieldfamily.co.uk>
2017-03-10 08:47:29 +0000

    removed react-date imports

--- client/app.js
+++ client/app.js
@@ -14,7 +14,6 @@ import {notify} from "react-notify-toast";
 import {default as rootReducer} from "./reducers";
 
 import "./styles/base.css";
-import 'react-dates/lib/css/_datepicker.css';
 
 const loggerMiddleware = createLogger()
 const middleware = applyMiddleware(thunkMiddleware, loggerMiddleware)
