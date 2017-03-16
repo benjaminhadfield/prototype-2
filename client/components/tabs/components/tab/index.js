2ac8e9920af2e75dd11e32afc2e969abc55ec0ad
Ben Hadfield <Ben@hadfieldfamily.co.uk>
2017-03-16 21:15:14 +0000

    tabs

--- client/components/tabs/components/tab/index.js
+++ client/components/tabs/components/tab/index.js
@@ -1,4 +1,9 @@
 import React from 'react';
 import styles from './styles.css';
+import classNames from 'classnames'
 
-export const Tab = (props) => <div className={styles.tab} {...props}/>
+export const Tab = ({isActive, ...props}) => (
+  <div
+    className={classNames(styles.tab, {[styles.active]: isActive})}
+    {...props}/>
+)
