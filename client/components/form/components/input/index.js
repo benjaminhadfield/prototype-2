719c9b7de2189ce4f61404e1753c493908cfc09d
Ben Hadfield <Ben@hadfieldfamily.co.uk>
2017-03-16 13:13:48 +0000

    began on form

--- client/components/form/components/input/index.js
+++ client/components/form/components/input/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import styles from './styles.css';
 
-export const Input = (props) => {
+export const Input = ({name, ...props}) => {
+  const ucFirst = (str) => str[0].toUpperCase() + str.slice(1)
   return (
-    <input
-      className={styles.input}
-      type="text"
-      placeholder={props.name}
-      {...props}/>
+    <div>
+      <label className={styles.label} htmlFor={name}>{ucFirst(name)}</label>
+      <input
+        id={name}
+        className={styles.input}
+        type="text"
+        nam={name}
+        placeholder={name}
+        {...props}/>
+    </div>
   );
 };
--- client/containers/Referrals/index.js
+++ client/containers/Referrals/index.js
@@ -1,14 +1,30 @@
-import React from 'react'
-import styles from './styles.css'
+import React from 'react';
+import styles from './styles.css';
 import {connect} from 'react-redux';
+import {Input, Select} from '../../components/form';
+import {Button} from '../../components/button';
 
 class Referrals extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    // post to API..
+  }
+
   render() {
     return (
-      <div>Referrals Form</div>
-
-    )
+      <form onSubmit={this.handleSubmit}>
+        <Input name="Field A"/>
+        <Input name="Field B"/>
+        <Select name="abc" options={[{label: 'values', value: 1}, 'values2']}/>
+        <Button type="submit" children="Submit"/>
+      </form>
+    );
   }
 }
 
-export default connect()(Referrals)
+export default connect()(Referrals);
