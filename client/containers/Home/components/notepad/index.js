4a58cb1a3522db42e66b78bc5392ce173a55770a
JhumanJ <julien@nahum.net>
2017-03-21 12:12:16 +0000

    try scheduler api fix without q

---
+++ client/containers/Home/components/notepad/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './styles.css';
+import {Col,FormGroup,ControlLabel,FormControl,Button,Panel} from 'react-bootstrap';
+
+export class NotePad extends React.Component {
+  render() {
+    return (
+        <div>
+            
+        </div>
+    )
+  }
+};
